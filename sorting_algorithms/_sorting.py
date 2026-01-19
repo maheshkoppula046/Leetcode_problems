369cd295c6e396d34a847f1850a8069cef81aae3
KOPPULA <mahesh.koppula-ext@hpe.com>
2026-01-19 13:26:49 +0530

    merge sort added

--- sorting_algorithms/_sorting.py
+++ sorting_algorithms/_sorting.py
@@ -23,7 +23,32 @@ class Solution:
 
 
 
+    def merge_sort(self,arr):
+        if len(arr)<=1:
+            return arr
+        mid = len(arr)//2
+        left = self.merge_sort(arr[:mid])
+        right = self.merge_sort(arr[mid:])
+        return self.merge(left,right)
+    
+    def merge(self,left,right):
+        result =[]
+        i=j=0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                result.append(left[i])
+                i+=1
+            else:
+                result.append(right[j])
+                j+=1
+            
+        result+=left[i:]
+        result+=right[j:]
+        return result
+
+
 
 arr = [3,2,4,5,1,9,0]
 print(Solution().bubble_sort(arr))
 print(Solution().select_sort(arr))
+print(Solution().merge_sort(arr))
